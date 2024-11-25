2e9438649c975969bf98151f187c43ad43a21ffe
Sridhar Balasubramanian <sridhar.balasubramanian@criteriacorp.com>
2024-11-25 12:40:01 +0530

    env set up

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const Config = {
-    apiUrl: process.env.REACT_APP_API_URL || "http://localhost:3000",
-    port: process.env.REACT_APP_PORT || 3001,
+    apiUrl: process.env.REACT_APP_API_URL || "https://auth-api.fullstackdev.in",
+    port: process.env.REACT_APP_PORT || 80,
 };
 
 export default Config;
